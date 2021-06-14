739219629e5370da86fdff741fb8e860ab5322bb
TanishqSehgal7 <tsgl7246@gmail.com>
2021-06-15 02:08:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/alarmfeaturewithdateandtimepicker/BroadcastReceiver.kt
@@ -0,0 +1,38 @@
+package com.example.alarmfeaturewithdateandtimepicker
+
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
+class BroadcastReceiver : BroadcastReceiver() {
+
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+
+        val intent=Intent(context,Main2Activity::class.java)
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        val pendingIntent=PendingIntent.getActivity(context,0,intent,0)
+
+
+        val notificationBuilder= context?.let { NotificationCompat.Builder(it,"alarmNotification") }
+        notificationBuilder?.setSmallIcon(R.drawable.ic_launcher_background)?.setContentTitle("Alarm Feature Test")
+            ?.setContentText("This is a test notification for your alarm")?.setAutoCancel(true)
+            ?.setDefaults(NotificationCompat.DEFAULT_ALL)?.priority = NotificationCompat.PRIORITY_HIGH
+        notificationBuilder?.setContentIntent(pendingIntent)
+
+
+        val notificationManager= context?.let { NotificationManagerCompat.from(it) }
+        if (notificationBuilder != null) {
+            notificationManager?.notify(123,notificationBuilder.build())
+        }
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/alarmfeaturewithdateandtimepicker/MainActivity.kt
@@ -0,0 +1,108 @@
+package com.example.alarmfeaturewithdateandtimepicker
+
+import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.alarmfeaturewithdateandtimepicker.databinding.ActivityMainBinding
+import com.google.android.material.datepicker.CalendarConstraints
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
+import java.time.Clock
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var viewBinding: ActivityMainBinding
+    lateinit var timePicker:MaterialTimePicker
+    lateinit var calendar: Calendar
+    lateinit var alarmManager: AlarmManager
+    lateinit var pendingIntent: PendingIntent
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        viewBinding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        // Create Notification Channel
+        CreateNotificationChannel()
+
+        viewBinding.setAlarm.setOnClickListener {
+
+           SetAlarm()
+
+        }
+
+        viewBinding.TimePicker.setOnClickListener {
+            ShowDateAndTimePicker()
+        }
+//        CacelAlarm()
+    }
+
+    fun CreateNotificationChannel() {
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channelName:CharSequence = "MyAlarmNotificationChannel"
+            val description:String="Channel for my Alarm Notification"
+            val importance:Int=NotificationManager.IMPORTANCE_HIGH
+            val notificationChannel= NotificationChannel("alarmNotification",channelName,importance)
+            notificationChannel.description=description
+
+            val notificationManager:NotificationManager=getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(notificationChannel)
+
+        }
+    }
+
+    fun ShowDateAndTimePicker() {
+        timePicker=MaterialTimePicker.Builder()
+            .setTimeFormat(TimeFormat.CLOCK_12H)
+            .setHour(12)
+            .setMinute(0)
+            .setTitleText("Select Time for Alarm")
+            .build()
+
+        timePicker.show(supportFragmentManager,"alarmNotification")
+        timePicker.addOnPositiveButtonClickListener {
+            if (timePicker.hour>12){
+                viewBinding.SelectedTime.text=(timePicker.hour-12).toString() + " : " + timePicker.minute.toString()
+            }
+            else {
+                viewBinding.SelectedTime.text=timePicker.hour.toString() + " : " + timePicker.minute.toString()
+            }
+
+            calendar= Calendar.getInstance()
+            calendar.set(Calendar.HOUR_OF_DAY,timePicker.hour)
+            calendar.set(Calendar.MINUTE,timePicker.minute)
+            calendar.set(Calendar.SECOND,0)
+            calendar.set(Calendar.MILLISECOND,0)
+
+        }
+    }
+
+    fun SetAlarm(){
+        alarmManager=getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent=Intent(this,BroadcastReceiver::class.java)
+        pendingIntent= PendingIntent.getBroadcast(this,0,intent,0)
+        alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP,calendar.timeInMillis,AlarmManager.INTERVAL_HALF_DAY,pendingIntent)
+        Toast.makeText(this,"Alarm Set Successfully",Toast.LENGTH_SHORT).show()
+    }
+
+    fun CacelAlarm() {
+        pendingIntent= PendingIntent.getBroadcast(this,0,intent,0)
+        if (alarmManager==null){
+            alarmManager=getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        }
+        alarmManager.cancel(pendingIntent)
+    }
+
+}
